ca8d5fc9398b117291d0e92ef089f5874bbc5080
Kenso Trabing <kenso.trabing@bloomwebsite.com>
2016-09-05 15:45:48 -0400

    commenting out test; it works: local/stage/production

--- app.py
+++ app.py
@@ -13,5 +13,5 @@ def hello_name(name):
         return "Hello {}!".format(name)
 
 if __name__ == "__main__":
-        print(os.environ["APP_SETTINGS"])
+#        print(os.environ["APP_SETTINGS"])
         app.run()
